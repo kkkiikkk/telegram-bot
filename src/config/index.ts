e651703eac3a14c6546ddedabfaca6c3e4b96c22
kkkiikkk <ruddnovskiy@gmail.com>
2022-09-22 12:44:26 +0300

    fix problems

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,7 @@ dotenv.config()
 
 export default {
     TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN,
-    DB_URL: process.env.DB_URL
+    DB_URL: process.env.DB_URL,
+    ALERT_LINK: process.env.ALERT_LINK,
+    AREA_OF_RESIDENCE: process.env.AREA_OF_RESIDENCE
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 // Core
 import TelegramBot from 'node-telegram-bot-api'
+import axios from 'axios';
+const cron = require('node-cron')
 
 // Config
 import config from './config'
@@ -9,8 +11,37 @@ import { copyMessageHandler, uploadInfo } from './handlers'
 
 // Db
 import { connectDb } from './mongo'
+import { Message } from './mongo/schemas/message.scema'
 
 const bot = new TelegramBot( config.TELEGRAM_TOKEN, { polling: true } )
 connectDb().catch(err => console.log(err))
 bot.onText(/^\/upload/, async (msg) => uploadInfo(msg, bot));
-bot.onText(/^\/get/, async (msg) => copyMessageHandler(msg, bot))
+bot.onText(/^\/get/, async (msg) => copyMessageHandler(msg, bot));
+let prevState: boolean = true;
+cron.schedule('*/10 * * * * *', async () => {
+    try {
+        let res = await axios.get(config.ALERT_LINK);
+        if (res.data && 'states' in res.data && config.AREA_OF_RESIDENCE in res.data.states) {
+            let zpState = res.data.states[config.AREA_OF_RESIDENCE];
+            console.log('zpState', zpState.enabled)
+            if (zpState.enabled !== prevState) {
+                const messages = await Message.find().distinct('chatId')
+                console.log('messages', messages)
+                for (const message of messages) {
+                    if(zpState.enabled) {
+                        console.log('Тревога')
+                        await bot.sendMessage(message, '‼️‼️‼️‼️‼️‼️ТРЕВОГА‼️‼️‼️‼️‼️‼️')
+                    }
+                    if (!zpState.enabled){
+                        console.log('Отбой')
+                        await bot.sendMessage(message, '👍👍👍ОТБОЙ👍👍👍')
+                    }
+                }
+            }
+            prevState = zpState.enabled;
+        }
+    }
+    catch (e) {
+        console.error('[ERROR]', e)
+    }
+})
